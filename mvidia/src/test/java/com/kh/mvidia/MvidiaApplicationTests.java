b460d4beb00ba6ecc84aeb867b71010a6d1a8bbd
usy <uhsuyong@gmail.com>
2025-09-03 15:35:56 +0900

    mvidia
    mvidia

---
+++ mvidia/src/test/java/com/kh/mvidia/MvidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.mvidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

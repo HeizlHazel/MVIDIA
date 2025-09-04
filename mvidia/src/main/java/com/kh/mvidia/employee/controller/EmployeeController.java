5e5f53787ee530c009032007d92eff305bf33f2e
usy <uhsuyong@gmail.com>
2025-09-04 23:26:02 +0900

    login 기능

--- mvidia/src/main/java/com/kh/mvidia/employee/controller/EmployeeController.java
+++ mvidia/src/main/java/com/kh/mvidia/employee/controller/EmployeeController.java
@@ -1,8 +1,20 @@
 package com.kh.mvidia.employee.controller;
 
+import com.kh.mvidia.employee.model.service.EmployeeService;
+import com.kh.mvidia.employee.model.vo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService empService;
+
+    @PostMapping("login.emp")
+    public String loginEmp(Employee emp){
+        int result =  empService.loginEmp(emp);
+        return "/common/mainPage";
+    }
 }

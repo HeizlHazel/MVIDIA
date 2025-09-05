97faf58dab1be8d139ee8df632ab56fa9b1a5288
hye <hyejee0221@gmail.com>
2025-09-05 17:39:20 +0900

    ➕
    관리자 메뉴 mapping 기능 추가

--- mvidia/src/main/java/com/kh/mvidia/common/controller/HomeController.java
+++ mvidia/src/main/java/com/kh/mvidia/common/controller/HomeController.java
@@ -11,11 +11,41 @@ public class HomeController {
 	public String index(){
 		return "common/loginPage";
 	}
-	
+
 	@GetMapping("/main")
 	public String main(Model model) {
 		model.addAttribute("sessionSeconds", 3600);
 		return "common/mainPage";
 	}
-	
+
+
+    
+
+    /* 관리자 대시보드 */
+    @GetMapping("/admin")
+    public String adminDashboard(Model model) {
+        model.addAttribute("sessionSeconds", 3600);
+        return "admin/adminDashboard";
+    }
+    /* 계정 권한 관리 */
+    @GetMapping("/permission")
+    public String grantRevoke(Model model) {
+        model.addAttribute("sessionSeconds", 3600);
+        return "admin/grantRevokePermission";
+    }
+
+    /* ERP UI 설정 */
+    @GetMapping("/ui")
+    public String uiSettings(Model model) {
+        model.addAttribute("sessionSeconds", 3600);
+        return "admin/uiSettings";
+    }
+
+    /* 사용자 계정 잠금 해제 */
+    @GetMapping("/unlock")
+    public String unlockAccount(Model model) {
+        model.addAttribute("sessionSeconds", 3600);
+        return "admin/unlockAccount";
+    }
+
 }

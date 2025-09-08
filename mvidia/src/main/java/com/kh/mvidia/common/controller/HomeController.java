551b59cf560d6d430c2ff83149e7d06741bb1db2
hye <hyejee0221@gmail.com>
2025-09-08 11:06:56 +0900

    📄 관리자 메뉴 매핑 이동 및 삭제

--- mvidia/src/main/java/com/kh/mvidia/common/controller/HomeController.java
+++ mvidia/src/main/java/com/kh/mvidia/common/controller/HomeController.java
@@ -18,34 +18,4 @@ public class HomeController {
 		return "common/mainPage";
 	}
 
-
-
-
-    /* 관리자 대시보드 */
-    @GetMapping("/admin")
-    public String adminDashboard(Model model) {
-        model.addAttribute("sessionSeconds", 3600);
-        return "admin/adminDashboard";
-    }
-    /* 계정 권한 관리 */
-    @GetMapping("/permission")
-    public String grantRevoke(Model model) {
-        model.addAttribute("sessionSeconds", 3600);
-        return "admin/grantRevokePermission";
-    }
-
-    /* ERP UI 설정 */
-    @GetMapping("/ui")
-    public String uiSettings(Model model) {
-        model.addAttribute("sessionSeconds", 3600);
-        return "admin/uiSettings";
-    }
-
-    /* 사용자 계정 잠금 해제 */
-    @GetMapping("/unlock")
-    public String unlockAccount(Model model) {
-        model.addAttribute("sessionSeconds", 3600);
-        return "admin/unlockAccount";
-    }
-
 }

186827c64625687e4c46027bd56e1b0294bb0b6c
hye <hyejee0221@gmail.com>
2025-09-08 17:43:02 +0900

    ➕ 권한 관리 Dao 추가

---
+++ mvidia/src/main/java/com/kh/mvidia/Permission/model/dao/PermissionDao.java
@@ -0,0 +1,16 @@
+package com.kh.mvidia.Permission.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import com.kh.mvidia.Permission.model.vo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+@Repository
+public class PermissionDao {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    public Employee selectEmp(String empNo) {
+        return sqlSession.selectOne("permissionMapper.selectEmp", empNo);
+    }
+}

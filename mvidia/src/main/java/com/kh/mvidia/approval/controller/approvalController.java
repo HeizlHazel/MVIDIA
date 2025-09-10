21e14204a1080a2bec5c1179c9c36cb77f880380
hye <hyejee0221@gmail.com>
2025-09-10 12:04:11 +0900

    ➕ 전자결재 페이지 매핑 추가

--- mvidia/src/main/java/com/kh/mvidia/approval/controller/approvalController.java
+++ mvidia/src/main/java/com/kh/mvidia/approval/controller/approvalController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class approvalController {
 
-    @GetMapping("approval")
+    /* 전자결재 신청 */
+    @GetMapping("approvalform")
+    public String approvalForm(Model model) {
+        model.addAttribute("sessionSeconds", 3600);
+        return "approval/approvalForm";
+    }
+
+    /* 전자결재 문서함 */
+    @GetMapping("approvalbox")
     public String approvalBox(Model model) {
         model.addAttribute("sessionSeconds", 3600);
         return "approval/approvalBox";

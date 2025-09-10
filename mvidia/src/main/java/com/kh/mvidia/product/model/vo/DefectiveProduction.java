ec400804fa21f5b342908371b5de5ad4fcdaa86c
pha <fkqpsejzltm@naver.com>
2025-09-10 19:59:48 +0900

    📄 defQty 타입 수정, occurDate 보완

--- mvidia/src/main/java/com/kh/mvidia/product/model/vo/DefectiveProduction.java
+++ mvidia/src/main/java/com/kh/mvidia/product/model/vo/DefectiveProduction.java
@@ -1,6 +1,7 @@
 package com.kh.mvidia.product.model.vo;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
 
@@ -13,8 +14,9 @@ public class DefectiveProduction {  // 제품불량등록 테이블
 
     private String defNo;      // 불량 아이디
     private String prodCode;   // 제품 코드 (생산제품정보 테이블의 prodCode 참조)
-    private int defQty;        // 불량 수량
+    private Integer defQty;        // 불량 수량
     private String defCode;    // 불량 코드 (불량유형 테이블의 defCode 참조)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date occurDate;    // 발생일
     private String defStatus;  // 상태 (상태 테이블의 status)
     private String remarks;    // 비고

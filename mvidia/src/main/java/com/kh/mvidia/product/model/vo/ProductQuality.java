53529acd1b6bfbbc33e6b5579fc30807792cc596
pha <fkqpsejzltm@naver.com>
2025-09-11 20:53:17 +0900

    ➕ 매개변수 추가

--- mvidia/src/main/java/com/kh/mvidia/product/model/vo/ProductQuality.java
+++ mvidia/src/main/java/com/kh/mvidia/product/model/vo/ProductQuality.java
@@ -15,4 +15,6 @@ public class ProductQuality { // 생산제품정보 테이블
     private int price;         // 단가
     private String localCode;  // 지역 코드 -> 재고 수량(stock) 참조
     private String prodStatus; // 상태 -> 상태 테이블 참조
+    private int stock; // 재고 수량 (재고 테이블의 local_code join으로 가져오기)
+    private String statusName; // 처리상태명 (상태 테이블의 status join으로 가져오기)
 }

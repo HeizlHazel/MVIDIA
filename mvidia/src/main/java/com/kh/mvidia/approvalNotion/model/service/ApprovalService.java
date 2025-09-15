14a53d8e9d17ce9d4ba80f116373a67953bd9303
hye <hyejee0221@gmail.com>
2025-09-15 18:16:13 +0900

    ➕ 노션 연동 메서드 추가

--- mvidia/src/main/java/com/kh/mvidia/approvalNotion/model/service/ApprovalService.java
+++ mvidia/src/main/java/com/kh/mvidia/approvalNotion/model/service/ApprovalService.java
@@ -9,7 +9,6 @@ public interface ApprovalService {
     // 전자결재 게시판 조회
     public String getDatabase();
 
-    // 전자결재 신청(게시판 등록)
-    HttpResponse<JsonNode> addApproval(String title, String category, String writer);
-
+    // 전자결재 등록(노션 연동)
+    HttpResponse<JsonNode> addPage(String applyWriter, String applyDept, String applyDate, String applyTitle, String approval, String details);
 }

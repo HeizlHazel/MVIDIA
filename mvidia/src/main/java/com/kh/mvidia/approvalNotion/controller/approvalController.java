52d59f2929a36d83f08fe59ca10c9768958a41e2
hye <hyejee0221@gmail.com>
2025-09-15 18:16:05 +0900

    ➕ 노션 연동 컨트롤러 추가

--- mvidia/src/main/java/com/kh/mvidia/approvalNotion/controller/approvalController.java
+++ mvidia/src/main/java/com/kh/mvidia/approvalNotion/controller/approvalController.java
@@ -1,29 +1,62 @@
 package com.kh.mvidia.approvalNotion.controller;
 
 import com.kh.mvidia.approvalNotion.model.service.ApprovalServiceImpl;
+import kong.unirest.JsonNode;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.Map;
 
 @Controller
 public class approvalController {
 
     @Autowired
     private ApprovalServiceImpl aService;
+]
 
-    /* 전자결재 신청 */
-    @GetMapping("approvalform")
+    /* 전자결재 신청 */@GetMapping("approvalform")
     public String approvalForm(Model model) {
         model.addAttribute("sessionSeconds", 3600);
-        return "approval/approvalForm";
+        return "approval/approvalNotionForm";
     }
 
     /* 전자결재 문서함 */
     @GetMapping("approvalbox")
     public String approvalBox(Model model) {
         model.addAttribute("sessionSeconds", 3600);
-        return "approval/approvalBox";
+        return "approval/approvalNotionBox";
+    }
+
+    @ResponseBody
+    @PostMapping(value="add.notion", consumes="application/json")
+    public String addNotion(@RequestBody Map<String, String> map) {
+
+        String applyWriter = map.get("applyWriter");
+        String applyDept = map.get("applyDept");
+        String applyDate = map.get("applyDate");
+        String applyTitle = map.get("applyTitle");
+        String approval = map.get("approval");
+        String details = map.get("details");
+
+        HttpResponse<JsonNode> response = aService.addPage(applyWriter, applyDept, applyDate, applyTitle, approval, details);
+
+       int status = response.getStatus();
+        if(status == 200) {
+            return "success";
+        }else {
+            return "fail";
+        }
+    }
+
+    @GetMapping("list.notion")
+    public String selectNotion(Model model) {
+        String dbData = aService.getDatabase();
+        model.addAttribute("dbData", dbData);
+        return "notion/notionListView";
     }
 
 }

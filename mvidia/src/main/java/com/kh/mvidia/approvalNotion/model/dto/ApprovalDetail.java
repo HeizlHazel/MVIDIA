5e8bafa4aea91bda2ea59dc38b34e1ea29d80a11
hye <hyejee0221@gmail.com>
2025-09-17 21:45:20 +0900

    ➕ 전자결재 상세 dto 추가

---
+++ mvidia/src/main/java/com/kh/mvidia/approvalNotion/model/dto/ApprovalDetail.java
@@ -0,0 +1,20 @@
+package com.kh.mvidia.approvalNotion.model.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ApprovalDetail {
+
+    private String id;        // 노션 페이지 ID
+    private String title;     // 제목
+    private String content;   // 본문(내용)
+    private String category;  // 구분
+    private String status;    // 결재상태
+    private String createdDate; // 작성일
+    private String approvers;
+
+}

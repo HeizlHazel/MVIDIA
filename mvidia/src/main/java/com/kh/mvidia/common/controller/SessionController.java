8807441a9167caf072f0f9ec2d72e812801948c4
usy <uhsuyong@gmail.com>
2025-09-19 17:16:46 +0900

    mvidia_seal 이미지
    mvidia_seal 이미지

--- mvidia/src/main/java/com/kh/mvidia/common/controller/SessionController.java
+++ mvidia/src/main/java/com/kh/mvidia/common/controller/SessionController.java
@@ -12,7 +12,6 @@ public class SessionController {
 	@ResponseBody
 	public String extendSession(HttpSession session) {
 		// 현재 세션의 마지막 접근 시간을 갱신하여 세션 만료 시간을 연장
-		session.invalidate(); // 이 부분을 주석 처리하고
 		session.setAttribute("justToRenewSession", System.currentTimeMillis()); // 이처럼 더미 속성을 추가하면 세션이 갱신됩니다.
 		return "success";
 	}

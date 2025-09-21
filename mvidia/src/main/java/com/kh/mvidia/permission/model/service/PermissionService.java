ff766a10ebb127b0ca00e54f902649fc21cb5672
HeizlHazel <hyejee0221@gmail.com>
2025-09-21 15:53:08 +0900

    🧑‍💻 권한, 전자결재 로그 메서드 인터페이스

--- mvidia/src/main/java/com/kh/mvidia/permission/model/service/PermissionService.java
+++ mvidia/src/main/java/com/kh/mvidia/permission/model/service/PermissionService.java
@@ -1,9 +1,12 @@
 package com.kh.mvidia.permission.model.service;
 
+import com.kh.mvidia.common.model.vo.PageInfo;
 import com.kh.mvidia.employee.model.vo.Employee;
 import com.kh.mvidia.permission.model.vo.Permission;
+import com.kh.mvidia.permission.model.vo.SystemLog;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PermissionService {
 
@@ -17,6 +20,21 @@ public interface PermissionService {
     void updatePermission(String empNo, List<String> list);
 
     // 권한 가져와서 세션에 저장
-    public List<Permission> getUserGrantedPermissions(String empNo);
+    List<Permission> getUserGrantedPermissions(String empNo);
+
+    // 권한 변경 이력 DB에 로그 저장
+    void savePermissionLog(String targetEmpNo, String actorEmpNo, List<Permission> beforePerms, List<String> afterPermCodes);
+
+    // 권한 로그 개수
+    int getPermissionLogListCount();
+
+    // 권한 로그 목록
+    List<Map<String, Object>> getPermissionLogList(PageInfo pi);
+
+    // 전자 결재 로그 개수
+    int getApprovalLogListCount();
+
+    // 전자 결재 목록
+    List<Map<String, Object>> getApprovalLogList(PageInfo pi);
 
 }
